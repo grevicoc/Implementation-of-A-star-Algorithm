ea6cd42863f2e117a06c2b762ac2c188d964e200
Muhammad Tito P <cocabc1@gmail.com>
2021-04-06 11:40:58 +0700

    filling distance done

--- header.py
+++ header.py
@@ -1,3 +1,5 @@
+import math
+
 def convertTxtToDict(listOfDict):
     namaFile = input("Masukkan nama file yang ingin diolah: ")
     objectFile = open(namaFile,'r')
@@ -17,12 +19,43 @@ def convertTxtToDict(listOfDict):
         tempDict[namaNode] = (xCoordinate,yCoordinate)
 
         #ekstrak simpul-simpul tetangga dari suatu node
+        tempDictForEdge = {}
         listOfEdge = line.split(' ')[2].split(',')
         for edge in listOfEdge:
             edge = edge.strip('\n')
-            tempDict[edge] = float(0)
-        
+            tempDictForEdge[edge] = float(0)
+        tempDict['edge'] = tempDictForEdge
+
         #masukkan ke dalam listOfDict
         listOfDict.append(tempDict)
-    
-    return
+
+def haversineDistance(lat1,lon1,lat2,lon2):
+    rad = math.pi/180
+    radius = 6371
+    dLat = rad * (lat2-lat1)
+    dLon = rad * (lon2-lon1)
+
+    a =  math.sin(dLat/2) * math.sin(dLat/2) + math.cos(rad*lat1) * math.cos(rad*lat2) *  math.sin(dLon/2) * math.sin(dLon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return radius*c
+
+def isiJarakAntarNode(listOfDict):
+    for i in range (len(listOfDict)):
+        
+        #ambil node source
+        nodeSrc = list(listOfDict[i].keys())[0]
+        for key in listOfDict[i]['edge']:
+
+            #ambil latitude and longitude dari nodeSrc
+            latSrc = listOfDict[i][nodeSrc][0]
+            lonSrc = listOfDict[i][nodeSrc][1]
+            
+            #ambil node destination
+            for dictVal in listOfDict:
+                if key in dictVal:
+                    #ambil latitude and longitude dari nodeDst
+                    latDst = dictVal[key][0]
+                    lonDst = dictVal[key][1]
+                    listOfDict[i]['edge'][key] = haversineDistance(latSrc,lonSrc,latDst,lonDst)
+                    break
+            
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import header
 if __name__ == "__main__":
     listOfdict = []
     header.convertTxtToDict(listOfdict)
-    # for dictVal in listOfdict:
-    #     print(dictVal)
+    header.isiJarakAntarNode(listOfdict)
+    for dictVal in listOfdict:
+        print(dictVal)
+    #listOfDict telah terisi namun jarak antar 2 node masih kosong
